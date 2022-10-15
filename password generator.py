1c623b8d0ed74d3c38a74126ee413457db343b0c
Mark Gobriel <markgobriel@gmail.com>
2022-10-14 20:28:52 -0400

    initial commit

---
+++ password generator.py
@@ -0,0 +1,21 @@
+import random
+import string
+
+print("welcome to python password generator:)")
+
+length = int(input("how long do you want your password to be? "))
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+mix = lower + upper + num + symbols
+
+temp = random.sample(mix, length)
+
+password = "".join(temp)
+
+print(password)
+
+
